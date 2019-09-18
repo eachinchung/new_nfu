324d95d32a86a4440a33e3263eb911367c86d737
钟予乾 <EachinChung@gmail.com>
2019-09-18 20:50:49 +0800

    初始化仓库

--- nfu/models.py
+++ nfu/models.py
@@ -5,9 +5,8 @@ from nfu.extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10))
-    password = db.Column(db.String(50))
-    dormitory = db.Column(db.String(50))
-    dormitory_id = db.Column(db.Integer)
-    ticket_session = db.Column(db.String(50))
+    password = db.Column(db.String(255))
+    room_id = db.Column(db.Integer)
+    bus_session = db.Column(db.String(50))
 
 
