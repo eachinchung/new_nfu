8288f0bd6c02c975417c68c7e65fd650c1eeb31b
钟予乾 <EachinChung@gmail.com>
2019-09-26 14:55:47 +0800

    增加宿舍表 成绩表 课程表

--- nfu/models.py
+++ nfu/models.py
@@ -6,11 +6,11 @@ from nfu.extensions import db
 # 用户表
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10))
-    password = db.Column(db.String(94))
+    name = db.Column(db.String)
+    password = db.Column(db.String)
     room_id = db.Column(db.Integer)
-    email = db.Column(db.String(255))
-    bus_session = db.Column(db.String(50))
+    email = db.Column(db.String)
+    bus_session = db.Column(db.String)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -23,7 +23,16 @@ class User(db.Model):
 class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     validate_email = db.Column(db.Boolean, default=False)
+    bus_ticket = db.Column(db.Boolean, default=False)
     admin = db.Column(db.Boolean, default=False)
+
+
+# 宿舍表
+class Dormitory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    building = db.Column(db.String)
+    floor = db.Column(db.String)
+    room = db.Column(db.Integer)
 
 
 # 电费表
@@ -32,3 +41,44 @@ class Electric(db.Model):
     room_id = db.Column(db.Integer, index=True)
     value = db.Column(db.Float)
     time = db.Column(db.Date)
+
+
+# 总成绩表
+class TotalAchievements(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    get_credit = db.Column(db.Integer)
+    selected_credit = db.Column(db.Integer)
+    average_achievement = db.Column(db.Float)
+    average_achievement_point = db.Column(db.Float)
+
+
+# 成绩表
+class Achievement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    course_type = db.Column(db.String)
+    course_name = db.Column(db.String)
+    course_id = db.Column(db.String)
+    resit_exam = db.Column(db.Boolean)
+    credit = db.Column(db.Float)
+    achievement_point = db.Column(db.Float)
+    final_achievements = db.Column(db.Float)
+    total_achievements = db.Column(db.Float)
+    midterm_achievements = db.Column(db.Float)
+    practice_achievements = db.Column(db.Float)
+    peacetime_achievements = db.Column(db.Float)
+
+
+# 课程表
+class ClassSchedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    course_name = db.Column(db.String)
+    course_id = db.Column(db.String)
+    teacher = db.Column(db.String)
+    classroom = db.Column(db.String)
+    weekday = db.Column(db.Integer)
+    start_node = db.Column(db.Integer)
+    end_node = db.Column(db.Integer)
+    start_week = db.Column(db.Integer)
+    end_week = db.Column(db.Integer)
