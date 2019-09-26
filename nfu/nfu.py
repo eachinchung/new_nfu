0e5ae23e245b35c43fa83ccae74a925e7dbf4992
钟予乾 <EachinChung@gmail.com>
2019-09-26 15:54:48 +0800

    完成登陆教务系统

--- nfu/nfu.py
+++ nfu/nfu.py
@@ -14,7 +14,8 @@ def get_student_name(student_id: int, password: str) -> tuple:
     }
 
     try:
-        response = post_session.post(url, data=data)
+        # 因为教务系统经常抽风，故设置1秒超时
+        response = post_session.post(url, data=data, timeout=1)
         token = json.loads(response.text)['msg']
 
         if not token:
@@ -28,7 +29,7 @@ def get_student_name(student_id: int, password: str) -> tuple:
         data = {'jwloginToken': token}
 
         try:
-            response = post_session.post(url, data=data)
+            response = post_session.post(url, data=data, timeout=1)
             name = json.loads(response.text)['msg']['name']
 
             if not name:
@@ -38,3 +39,28 @@ def get_student_name(student_id: int, password: str) -> tuple:
 
         except (OSError, KeyError):
             return False, '教务系统错误，请稍后再试'
+
+
+# 登陆教务系统
+def get_jw_token(student_id: int) -> tuple:
+    # 基于教务系统的Bug，登陆直接提交空字符串就可以了
+    url = 'http://ecampus.nfu.edu.cn:2929/jw-privilegei/User/r-login'
+    post_session = requests.session()
+    data = {
+        'username': student_id,
+        'password': '',
+        'rd': ''
+    }
+
+    try:
+        response = post_session.post(url, data=data, timeout=1)
+        token = json.loads(response.text)['msg']
+
+        if not token:
+            return False, '不可预知错误，请稍后再试！'
+
+    except (OSError, json.decoder.JSONDecodeError):
+        return False, '教务系统错误，请稍后再试'
+
+    else:
+        return True, token
