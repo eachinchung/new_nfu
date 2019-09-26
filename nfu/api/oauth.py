8722d28f37ad5a72fc6a34b63a7e775768100711
钟予乾 <EachinChung@gmail.com>
2019-09-26 20:32:55 +0800

    增加重新发送激活邮件

--- nfu/api/oauth.py
+++ nfu/api/oauth.py
@@ -18,16 +18,28 @@ def get_token() -> str:
     user = User.query.get(user_id)
     user_power = Power.query.get(user_id)
 
-    # 首先验证账号是否激活，若账号未激活，以下操作无意义
-    if user is None or not user_power.validate_email:
-        return jsonify({'message': '账号暂未激活。'})
+    # 首先验证账号是否存在
+    if user is None:
+        return jsonify({'message': '账号不存在。'})
 
     if not user.validate_password(password):
-        return jsonify({'message': '账号密码错误'})
+        return jsonify({'message': '密码错误'})
+
+    # 验证账号是否激活，若账号未激活，往后操作无意义
+    if not user_power.validate_email:
+        data = {
+            'id': user.id,
+            'name': user.name,
+            'email': user.email,
+        }
+        return jsonify({
+            'message': '账号暂未激活。',
+            'refresh_email_token': generate_token(data, token_type='REFRESH_EMAIL_TOKEN')
+        })
 
     return jsonify({
         'message': 'success',
-        'access_token': generate_token({'id': user.id, 'power': user_power}),
+        'access_token': generate_token(user_power.get_dict()),
         'refresh_token': generate_token({'id': user.id}, token_type='REFRESH_TOKEN', expires_in=2592000)
     })
 
@@ -61,6 +73,28 @@ def sign_up() -> str:
         return jsonify({'message': '该账号已存在'})
 
 
+# 重新发送激活邮件
+@oauth_bp.route('/refresh_validate_email', methods=['POST'])
+def refresh_validate_email() -> str:
+    token = request.form.get('refresh_validate_email_token')
+    validate = validate_token(token, 'REFRESH_EMAIL_TOKEN')
+
+    if validate[0]:
+        user_power = Power.query.get(validate[1]['id'])
+
+        if user_power is None:
+            return jsonify({'message': '账号不存在'})
+
+        if not user_power.validate_email:
+            token = generate_token({'user_id': validate[1]['id']}, token_type='EMAIL_TOKEN')
+            send_validate_email(validate[1]['email'], validate[1]['name'], validate[1]['id'], token)
+            return jsonify({'message': 'success'})
+
+        return jsonify({'message': 'success'})
+    else:
+        return jsonify({'message': validate[1]})
+
+
 # 刷新令牌
 @oauth_bp.route('/refresh_token', methods=['POST'])
 def refresh_token() -> str:
@@ -75,7 +109,7 @@ def refresh_token() -> str:
 
         return jsonify({
             'message': 'success',
-            'access_token': generate_token({'id': user.id, 'power': user_power}),
+            'access_token': generate_token(user_power.get_dict()),
             'refresh_token': generate_token({'id': user.id}, token_type='REFRESH_TOKEN', expires_in=2592000)
         })
     else:
