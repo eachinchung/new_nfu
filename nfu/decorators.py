3d795313c024b8eafe41bc5405f86e6e0add3bd2
钟予乾 <EachinChung@gmail.com>
2019-10-01 00:46:06 +0800

    检查用户是否登陆

---
+++ nfu/decorators.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+from flask import request
+
+from nfu.models import User
+from nfu.token import validate_token
+
+
+def check_login(func):
+    """
+    检查用户的access_token是否合法
+    :param func: 使用此装饰器的函数
+    :return: 指向新函数
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kw):
+        token = request.form.get('access_token')
+        validate = validate_token(token)
+
+        # 验证 token 是否通过
+        if validate[0]:
+            user = User.query.get(validate[1]['id'])
+            error = None
+
+        else:
+            user = None
+            error = validate[1]
+
+        return func(user, error, *args, **kw)
+
+    return wrapper
