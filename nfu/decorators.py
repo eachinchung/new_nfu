e1969f06eda1d4d8a27bc12339cf1783673eddd5
钟予乾 <EachinChung@gmail.com>
2019-10-02 18:15:49 +0800

    检查用户否具有校车功能的权限

--- nfu/decorators.py
+++ nfu/decorators.py
@@ -11,7 +11,7 @@ def check_access_token(func):
     检查用户的access_token是否合法
     因为有账号才能拿到token，故不考虑，账号不存在的情况
 
-    使用flask的g对象，全局储存，user数据
+    使用flask的g对象，全局储存，user、user_power数据
 
     :param func: 使用此装饰器的函数
     :return: 指向新函数，或者返回错误
@@ -33,4 +33,21 @@ def check_access_token(func):
 
         return func(*args, **kw)
 
+    return wrapper
+
+
+def check_power_school_bus(func):
+    """
+    检查用户否具有校车功能的权限
+
+    :param func: 使用此装饰器的函数
+    :return: 指向新函数，或者返回错误
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kw):
+        if not g.user_power['school_bus']:
+            return jsonify({'message': '没有访问权限'}), 403
+
+        return func(*args, **kw)
     return wrapper
