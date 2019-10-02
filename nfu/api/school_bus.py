d80e59829374df3279a444ace7ae1b6710a25b92
钟予乾 <EachinChung@gmail.com>
2019-10-02 17:58:07 +0800

    完成 获取班车时刻表

--- nfu/api/school_bus.py
+++ nfu/api/school_bus.py
@@ -1,3 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, request, g, jsonify
+
+from nfu.decorators import check_access_token
+from nfu.school_bus import get_ticket_schedule
 
 school_bus_bp = Blueprint('school_bus', __name__)
+
+
+@school_bus_bp.route('/get_schedule', methods=['POST'])
+@check_access_token
+def get_schedule():
+    """
+    获取班车时刻表
+    :return:
+    """
+
+    if not g.user_power['school_bus']:  # 检测是否有校车功能的权限
+        return jsonify({'message': '没有访问权限'}), 403
+
+    route_id = request.form.get('route_id')
+    date = request.form.get('date')
+
+    schedule = get_ticket_schedule(route_id, date, g.user.bus_session)
+    if not schedule[0]:
+        return jsonify({'message': schedule[1]}), 403
+
+    return jsonify({
+        'message': 'success',
+        'schedule': schedule[1],
+    })
---
+++ nfu/school_bus.py
@@ -0,0 +1,61 @@
+from json import loads, decoder
+from re import search
+
+from requests import session
+
+
+def get_ticket_schedule(route_id: int, date: list, bus_session: str) -> tuple:
+    """
+    若日期在车票预售期内，获取班车时刻表。
+
+    学校的校车系统，并没有做前后端分离，每天的班车数据都会在页面直接返回。
+    我们用正则抓取即可，同时只要提交日期就可以查看当天车票，并无太多限制。
+
+    - 字段说明
+        - route_id 路线id：南苑 -> 河堤公园：21，
+                          河堤公园 -> 南苑：22，
+                          南苑 -> 中大南校区：13，
+                          中大南校区 -> 南苑：14
+
+        - time     乘车日期
+
+    :param route_id: 校车路线
+    :param date: 乘车日期
+    :param bus_session: 校车系统的 session
+    :return: 0.是否成功获取数据，1.数据
+    """
+
+    url = 'http://nfuedu.zftcloud.com/campusbus_index/ticket/show_schedule.html'
+    http_session = session()
+    params = {
+        'route_id': route_id,
+        'time': date
+    }
+    headers = {'Cookie': bus_session}
+
+    try:
+        response = http_session.get(url, params=params, headers=headers)
+        data = loads(search(r'var msg = .+', response.text).group()[10:-1])
+    except (OSError, AttributeError, decoder.JSONDecodeError):
+        return False, '学校车票系统错误，请稍后再试'
+    else:
+        return True, data
+
+
+def get_passenger(bus_session: str) -> tuple:
+    """
+    获取乘车人数据
+    :param bus_session: 校车系统的 session
+    :return: 0.是否成功获取数据，1.数据
+    """
+    url = 'http://nfuedu.zftcloud.com/campusbus_index/my/passenger_puls.html'
+    headers = {'Cookie': bus_session}
+    http_session = session()
+
+    try:
+        response = http_session.get(url, headers=headers)
+        passenger = loads(search(r'var passenger = .+', response.text).group()[16:-1])
+    except (OSError, AttributeError, decoder.JSONDecodeError):
+        return False, '学校车票系统错误，请稍后再试'
+    else:
+        return True, passenger
