877b7a33dd175bbbd56d3352c709a6dea7b38af7
Eachin <EachinChung@gmail.com>
2019-11-03 17:32:29 +0800

    增加调起支付宝支付接口

--- nfu/api_bp/school_bus.py
+++ nfu/api_bp/school_bus.py
@@ -1,9 +1,10 @@
+from json import loads
+
 from flask import Blueprint, g, jsonify, render_template, request
 
 from nfu.NFUError import NFUError
 from nfu.common import check_access_token, check_power_school_bus
-from nfu.expand.school_bus import get_bus_schedule, get_passenger_data, get_ticket_data, get_ticket_ids, return_ticket, \
-    create_order
+from nfu.expand import school_bus
 from nfu.expand.token import validate_token
 from nfu.models import User
 
@@ -19,13 +20,17 @@ def get_schedule():
     :return:
     """
 
-    route_id = request.form.get('route_id')
-    date = request.form.get('date')
+    try:
+        data = loads(request.get_data().decode("utf-8"))
+        route_id = int(data['route_id'])
+        date = data['date']
+    except (TypeError, ValueError):
+        return jsonify({'adopt': False, 'message': '服务器内部错误'}), 500
 
     try:
-        schedule = get_bus_schedule(route_id, date, g.user.bus_session)
+        schedule = school_bus.get_bus_schedule(route_id, date, g.user.bus_session)
     except NFUError as err:
-        return jsonify({'adopt': False, 'message': err.message}), 500
+        return jsonify({'adopt': False, 'message': err.message})
 
     return jsonify({'adopt': True, 'message': schedule})
 
@@ -40,7 +45,7 @@ def get_passenger():
     """
 
     try:
-        passenger = get_passenger_data(g.user.bus_session)
+        passenger = school_bus.get_passenger_data(g.user.bus_session)
     except NFUError as err:
         return jsonify({'adopt': False, 'message': err.message}), 500
 
@@ -55,15 +60,19 @@ def create_order_bp():
     买票
     :return:
     """
-    passenger_ids = request.form.get('passenger_ids')
-    connect_id = request.form.get('connect_id')
-    schedule_id = request.form.get('schedule_id')
-    date = request.form.get('date')
-    take_station = request.form.get('take_station')
-    bus_session = g.user.bus_session
+    try:
+        data = loads(request.get_data().decode("utf-8"))
+        passenger_ids = data['passenger_ids']
+        connect_id = data['connect_id']
+        schedule_id = data['schedule_id']
+        date = data['date']
+        take_station = data['take_station']
+        bus_session = g.user.bus_session
+    except ValueError:
+        return jsonify({'adopt': False, 'message': '服务器内部错误'}), 500
 
     try:
-        order = create_order(passenger_ids, connect_id, schedule_id, date, take_station, bus_session)
+        order = school_bus.create_order(passenger_ids, connect_id, schedule_id, date, take_station, bus_session)
     except NFUError as err:
         return jsonify({'adopt': False, 'message': err.message, 'code': err.code}), 500
 
@@ -80,7 +89,7 @@ def get_ticket_ids_bp():
     """
 
     try:
-        ticket_ids = get_ticket_ids(request.form.get('order_id'), g.user.bus_session)
+        ticket_ids = school_bus.get_ticket_ids(request.form.get('order_id'), g.user.bus_session)
     except NFUError as err:
         return jsonify({'adopt': False, 'message': err.message}), 500
     return jsonify({'adopt': True, 'message': ticket_ids})
@@ -98,7 +107,7 @@ def return_ticket_bp():
     ticket_id = request.form.get('ticket_id')
 
     try:
-        post = return_ticket(order_id, ticket_id, g.user.bus_session)
+        post = school_bus.return_ticket(order_id, ticket_id, g.user.bus_session)
     except NFUError as err:
         return jsonify({'adopt': False, 'message': err.message}), 500
 
@@ -124,7 +133,7 @@ def get_ticket():
     order_id = request.args.get('order_id')
 
     try:
-        ticket_data = get_ticket_data(order_id, user.bus_session)
+        ticket_data = school_bus.get_ticket_data(order_id, user.bus_session)
     except NFUError as err:
         return jsonify({'adopt': False, 'message': err.message}), 500
 
@@ -134,3 +143,22 @@ def get_ticket():
         ticket=ticket_data[1],
         javascript=ticket_data[2]
     )
+
+
+@school_bus_bp.route('/alipay')
+def alipay():
+    """
+    调起支付宝支付
+    :return:
+    """
+    try:
+        token_data = validate_token(request.args.get('token'))
+    except NFUError as err:
+        return jsonify({'adopt': False, 'message': err.message}), 403
+
+    if not token_data['school_bus']:
+        return jsonify({'message': '没有访问权限'}), 403
+
+    trade_no = request.args.get('trade_no')
+
+    return render_template('html/alipay.html', trade_no=trade_no)
